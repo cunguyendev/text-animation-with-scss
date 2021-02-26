44b17b9c171537e943e28b7c20defa35f595cc3e
cunguyendev <cunguyen.dev@gmail.com>
2021-02-26 10:32:32 +0700

    Get the numer of lines from attribute.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   const ui = document.getElementById('ui');
+  const numberLines = ui.getAttribute('data-number-line');
 
-  for (let i = 0; i < 40; i += 1) {
+  for (let i = 0; i < +numberLines; i += 1) {
     const node = '<div class="text">Hello world!</div>';
 
     ui.innerHTML += node;
